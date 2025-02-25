4a8227932d41e92ab89e61099922a6fb3be22f61
stefan <128835276+histefan@users.noreply.github.com>
2025-02-25 18:59:53 +0100

    i2c device and controller interfaces

--- vp/src/platform/common/i2c_if.h
+++ vp/src/platform/common/i2c_if.h
@@ -10,10 +10,58 @@
 
 /* generic i2c device interface */
 class I2C_Device_IF {
-
+    virtual bool write(uint8_t data) = 0;
+    virtual uint8_t read() = 0;
 };
 
 /* i2c host interface */
 class I2C_IF {
 
+    std::map<uint8_t, I2C_Device_IF*> devices;
+
+    public: 
+
+    void register_device(uint8_t addr, I2C_Device_IF* device) {
+        devices.insert(std::pair<const uint32_t, I2C_Device_IF*>(addr, device));
+    }
+
+    void unregister_device(uint8_t addr) {
+        devices.erase(addr);
+    }
+
+    I2C_Device_IF *get_device(uint8_t addr) {
+        auto device = devices.find(addr);
+        if (device == devices.end()) {
+            return nullptr;
+        }
+        return device->second;
+    }
+
+    bool write(uint8_t addr, uint8_t data) {
+        I2C_Device_IF *device = get_device(addr);
+        bool ack = false
+		if (device == nullptr) {
+			std::cerr << "I2C: WARNING: Write to unregistered adress" << addr << std::endl;
+			return false;
+		}
+        ack = devices[addr]->write(data);
+        if (!ack) {
+            std::cerr << "I2C: WARNING: Write to device " << addr << " failed" << std::endl;
+            return false;
+        }
+		return true;
+    }
+
+    bool read(uint8_t addr, uint8_t &data) {
+        I2C_Device_IF *device = get_device(addr);
+        if (device == nullptr) {
+            std::cerr << "I2C: WARNING: Read from unregistered adress" << addr << std::endl;
+            return false;
+        }
+        data = devices[addr]->read();
+        return true;
+    }
+    
 };
+
+#endif /* RISCV_VP_I2C_IF_H */
