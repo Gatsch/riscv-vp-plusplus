c162ae6dd982695b94729e5d5d390df4f4679eb8
histefan <hi.stefan1410@gmail.com>
2025-02-25 17:08:22 +0100

    simple structure

---
+++ vp/src/platform/common/i2c_if.h
@@ -0,0 +1,19 @@
+#ifndef RISCV_VP_I2C_IF_H
+#define RISCV_VP_I2C_IF_H
+
+#include <map>
+#include <queue>
+#include <systemc>
+
+#include "core/common/irq_if.h"
+#include "util/tlm_map.h"
+
+/* generic i2c device interface */
+class I2C_Device_IF {
+
+};
+
+/* i2c host interface */
+class I2C_IF {
+
+};
